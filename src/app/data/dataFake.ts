2b8cff89f9f6d4b94d383ba39d3517c4f5eb2edc
Arthur Henrique <arthurmsantos2002@gmail.com>
2025-02-23 01:48:27 -0300

    Mudanças

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Goku",
+    "description": "Goku é o protagonista da franquia Dragon Ball, um guerreiro Saiyajin criado na Terra que busca constantemente superar seus limites. Ele atingiu o Instinto Superior durante o Torneio do Poder em Dragon Ball Super, após ser pressionado ao extremo em uma batalha contra Jiren, um dos oponentes mais fortes do multiverso. A técnica foi desbloqueada quando Goku absorveu a energia de uma explosão da Genki Dama e, combinado com seu instinto de sobrevivência e desejo de proteger seus amigos, atingiu um estado em que seu corpo reagia automaticamente aos ataques sem a necessidade de pensamento consciente.",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/goku-instinto-superior_qx99.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Gohan",
+    "description": "De um jovem tímido em Dragon Ball Z a um dos mais poderosos Z-Fighters em Dragon Ball Super, ele conquistou seu lugar no coração dos fãs. No filme Dragon Ball Super: Super Hero, Gohan alcançou um novo patamar com a transformação Gohan Beast, revelando um poder inédito.",
+    "photoCover": "https://www.einerd.com/wp-content/uploads/2025/01/dragon-ball-gohan-890x606.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Vegeta",
+    "description": "Vegeta, o príncipe dos Saiyajins, é conhecido por seu orgulho e desejo incessante de superar Goku. Em Dragon Ball Super, Vegeta desbloqueou a forma Ultra Ego, uma transformação ensinada por Bills, o Deus da Destruição. Essa forma aumenta o seu poder conforme ele aceita o dano em batalha, refletindo sua determinação feroz e natureza guerreira.",
+    "photoCover": "https://criticalhits.com.br/wp-content/uploads/2023/09/Ultra-Ego-Vegeta-1-1280x640.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Broly",
+    "description": "Broly é um Saiyajin de poder imenso, cuja fúria incontrolável o torna uma força imparável. Introduzido em Dragon Ball Super: Broly, ele foi treinado por seu pai Paragus, mas acabou despertando um poder que rivaliza até mesmo com Goku e Vegeta. Sua transformação em Super Saiyajin Lendário é movida por pura emoção e instinto.",
+    "photoCover": "https://preview.redd.it/toy1djvi7l3e1.jpeg?width=640&crop=smart&auto=webp&s=39c4444dfd017280cf0b65b833d0fdf25ee9da7e"
+  }
+];
